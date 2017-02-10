e50e4c6012eea67e7a74add615c1199ecd617118
gen <bigboxfoefox@gmail.com>
2017-02-10 21:33:42 +0300

    add promises

--- core/ECore.js
+++ core/ECore.js
@@ -15,78 +15,161 @@ const path = require('path')
 
 module.exports = class EServer {
   constructor() {
+  let path_dir = process.argv[1].split('/')
+  path_dir.pop()
+  path_dir = path_dir.join('/')
     this.essen = {
       app: express(),
-      path: process.cwd(),
+      path: path_dir,
     }
   }
   start() {
-    this.loadPlugins(err => {
-      if (err) return log.error(err)
-      EConfigurator.init(this.essen.path, (err, config) => {
-        log.level = config.log.level
-        if (err) return log.err(err)
-        log.debug('config loaded')
-        this.essen = Object.assign(this.essen, config)
-        EMiddleware.init(this.essen, err => {
-          if (err) log.err(err)
-          log.debug('middlewares inited')
-          EORM.init(err => {
-            if (err) log.err(err)
-            log.debug('ORM models inited')
-            EServiceman.init(this.essen, err => {
-              if (err) log.err(err)
-              log.debug('services inited')
-              this.bootstrap(err => {
-                if (err) log.err(err)
-                log.debug('bootstrap executed')
-                this.initRouter(err => {
-                  if (err) log.err(err)
-                  log.debug('routes inited')
-                  this.startServer()
-                });
-              });
-            });
+    this.loadPlugins()
+    .then(resolve => {
+      return this.initConfig(this)
+    })
+    .then(resolve => {
+      return this.initMiddleware(this)
+    })
+    .then(resolve => {
+      return this.initEORM(this)
+    })
+    .then(resolve => {
+      return this.initEserviceman(this)
+    })
+    .then(resolve => {
+      return this.bootstrap(this)
+    })
+    .catch(err => {
+      console.log('reject');
+    })
+  }
+  loadPlugins() {
+    return new Promise((resolve, reject) => {
+      try {
+        setTimeout(() => {
+          global.essen = this.essen;
+          global.log = new (winston.Logger)({
+            transports: [
+              new (winston.transports.Console)({
+                colorize: true,
+                timestamp: () => new Date().toTimeString().split(' ')[0],
+                formatter(options) {
+                  return options.timestamp()
+                    + ' - '
+                    + winston.config.colorize(options.level, options.level.toUpperCase())
+                    + ' - '
+                    + (options.message ? options.message : '')
+                    + (options.meta && Object.keys(options.meta).length ? '\n' + JSON.stringify(options.meta, null, 2) : '' );
+                },
+              }),
+            ],
           });
-        });
+          global.log.level = 'silly';
+          log.debug('Plugins loaded')
+        })
+        return resolve()
+      } catch(err) {
+        return reject(err)
+      }
+
+    })
+  }
+  initConfig(context) {
+    return new Promise((resolve, reject) => {
+      EConfigurator.init(context.essen.path, (err, config) => {
+        if (err) {
+          log.error('config load error')
+          reject(err)
+        } else {
+          log.level = config.log.level
+          context.essen = Object.assign(context.essen, config)
+          log.debug('config loaded')
+          resolve()
+        }
+      })
+    })
+  }
+  initMiddleware(context) {
+    return new Promise((resolve, reject) => {
+      EMiddleware.init(context.essen, err => {
+        if (err) {
+          log.error('middlewares init error', err)
+          reject(err)
+        } else {
+          log.debug('middlewares inited')
+          resolve()
+        }
+      })
+    })
+  }
+  initEORM(context) {
+    return new Promise((resolve, reject) => {
+      EORM.init(err => {
+        if (err) {
+          log.error('ORM models init error')
+          reject()
+        } else {
+          log.debug('ORM models inited')
+          resolve()
+        }
+      })
+    })
+  }
+  initEserviceman(context) {
+    return new Promise((resolve, reject) => {
+      EServiceman.init(this.essen, err => {
+        if (err) {
+          log.debug('services init error')
+          reject()
+        } else {
+          log.debug('services inited')
+          resolve()
+        }
+      })
+    })
+  }
+  bootstrap(context) {
+    return new Promise((resolve, reject) => {
+      try {
+        const config_path = path.join(essen.path, 'config/bootstrap.js');
+        const bootstrap = require(config_path);
+        log.debug('bootstrap executed')
+        resolve()
+      } catch(err) {
+        log.debug('bootstrap execute error')
+        reject()
+      }
+    })
+  }
+  initRouter(context) {
+    return new Promise((resolve, reject) => {
+      console.log(context.essen);
+      try {
+        const router = new ERouter(context.essen);
+        router.init(() => {
+          log.debug('routes inited')
+          resolve()
+        })
+      } catch(err) {
+        log.error('routes init error')
+        reject()
+      }
+    })
+  }
+  startServer(context) {
+    console.log('start!!');
+    return new Promise((resolve, reject) => {
+      context.essen.app.listen(context.essen.server.port, err => {
+        if (err) {
+          log.err('start server error');
+          reject()
+        } else {
+          log.info(`${context.essen.server.name} listening on port ${context.essen.server.port}`);
+          log.info(`using db ${context.essen.db.name} at ${context.essen.db.host}`);
+          resolve()
+        }
       });
-    });
-  }
-  loadPlugins(cb) {
-    global.essen = this.essen;
-    global.log = new (winston.Logger)({
-      transports: [
-        new (winston.transports.Console)({
-          colorize: true,
-          timestamp: () => new Date().toTimeString().split(' ')[0],
-          formatter(options) {
-            return options.timestamp()
-              + ' - '
-              + winston.config.colorize(options.level, options.level.toUpperCase())
-              + ' - '
-              + (options.message ? options.message : '')
-              + (options.meta && Object.keys(options.meta).length ? '\n' + JSON.stringify(options.meta, null, 2) : '' );
-          },
-        }),
-      ],
-    });
-    global.log.level = 'silly';
-    return cb();
-  }
-  bootstrap(cb) {
-    const config_path = path.join(essen.path, 'config/bootstrap.js');
-    const bootstrap = require(config_path);
-    bootstrap(cb);
-  }
-  initRouter(cb) {
-    const router = new ERouter(this.essen);
-    router.init(cb);
-  }
-  startServer() {
-    this.essen.app.listen(this.essen.server.port, err => {
-      if (err) log.err(err);
-      log.info(`${this.essen.server.name} listening on port ${this.essen.server.port}`);
-      log.info(`using db ${this.essen.db.name} at ${this.essen.db.host}`);
-    });
+    })
   }
 }
